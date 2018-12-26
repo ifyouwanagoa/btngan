362ea9f61acd995bd79e016f9c329116f1d56fee
ifyouwanagoa <42785783+ifyouwanagoa@users.noreply.github.com>
2018-12-26 23:29:52 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -292,27 +292,6 @@ client.on("message", message => {
 });
 //Toxic Codes
 
-client.on('message', message => {
-  if (true) {
-if (message.content === '#invite') {
-      message.author.send('  https://discord.gg/6eunhj  |  تفضل ربط البوت     ').catch(e => console.log(e.stack));
- 
-    }
-   }
-  });
- 
- 
-client.on('message', message => {
-     if (message.content === "#invite") {
-     let embed = new Discord.RichEmbed()
-  .setAuthor(message.author.username)
-  .setColor("#9B59B6")
-  .addField(" Done | تــــم" , " |  تــــم ارســالك في الخــاص")
-     
-     
-     
-  message.channel.sendEmbed(embed);
-    }
-});
+
 
 client.login(process.env.BOT_TOKEN);
