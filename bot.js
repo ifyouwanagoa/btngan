636e25bd479a3efdbb140165e588a0de5f33eb4b
ifyouwanagoa <42785783+ifyouwanagoa@users.noreply.github.com>
2018-12-27 14:34:02 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -101,20 +101,20 @@ var prefix = "#";
 
 client.on("ready", async  => {
 setInterval(function(){
-client.channels.find('id', '527586739552911382').setName("♥W");
-client.channels.find('id', '527586739552911382').setName("♥We");
-client.channels.find('id', '527586739552911382').setName("♥Wel");
-client.channels.find('id', '527586739552911382').setName("♥Welc");
-client.channels.find('id', '527586739552911382').setName("♥Welco");
-client.channels.find('id', '527586739552911382').setName("♥Welcom");
-client.channels.find('id', '527586739552911382').setName("♥Welcome");
-client.channels.find('id', '527586739552911382').setName("♥Welcome♥");
+client.channels.find('id', '527458463777947658').setName("♥W");
+client.channels.find('id', '527458463777947658').setName("♥We");
+client.channels.find('id', '527458463777947658').setName("♥Wel");
+client.channels.find('id', '527458463777947658').setName("♥Welc");
+client.channels.find('id', '527458463777947658').setName("♥Welco");
+client.channels.find('id', '527458463777947658').setName("♥Welcom");
+client.channels.find('id', '527458463777947658').setName("♥Welcome");
+client.channels.find('id', '527458463777947658').setName("♥Welcome♥");
   }, 3000);
 });
 
 client.on("ready", () => { // كود رينبو
   function lol() {
-    client.guilds.get('526505018682114057').roles.find("name", "Color").setColor("RANDOM");
+    client.guilds.get('471121072591470604').roles.find("name", "Color").setColor("RANDOM");
 
   };
   setInterval(lol, 5000);
