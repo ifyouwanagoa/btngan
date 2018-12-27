f76c724ffa207d017ff30067c23e16e6aa963c62
ifyouwanagoa <42785783+ifyouwanagoa@users.noreply.github.com>
2018-12-27 21:21:53 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -324,21 +324,6 @@ message.channel.sendEmbed(cat);
     }
 });
 
-client.on('message', message => {
-    if (message.content.startsWith(prefix + "avatar")) {
-        var mentionned = message.mentions.users.first();
-    var x5bzm;
-      if(mentionned){
-          var x5bzm = mentionned;
-      } else {
-          var x5bzm = message.author;
-          
-      }
-        const embed = new Discord.RichEmbed()
-        .setColor("RANDOM")
-        .setImage(`${x5bzm.avatarURL}`)
-      message.channel.sendEmbed(embed);
-    }
-});
+
 
 client.login(process.env.BOT_TOKEN);
